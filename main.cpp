1555bf3d3c9dac1f5166561bf2b7c453ae9fd47e
Benoit Holzer <benoit.holzer8@etu.univ-lorraine.fr>
2019-01-16 10:38:02 +0100

    remplir model avec des triangles de couleurs aléatoires

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green   = TGAColor(0, 255,   0,   255);
 
 std::vector<std::vector<int>> line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     std::vector<int> tabX, tabY;
@@ -69,14 +70,63 @@ std::vector<std::string> readFile(std::string file){
     return line;
 }
 
-void draw(model m, TGAImage &image, TGAColor color){
-    std::cout << "nverts : "+m.nverts() << std::endl;
+
+void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, TGAImage &image, TGAColor color){
+        if(y1 > y2) {
+            std::swap(y1,y2);
+            std::swap(x1,x2);
+        }
+        if(y1 > y3){
+            std::swap(y1,y3);
+            std::swap(x1,x3);
+        }
+        if(y2 > y3){
+            std::swap(y2,y3);
+            std::swap(x2,x3);
+        }
+        int height = y3 - y1;
+        if(height != 0) {
+            for (int i = y1; i <= y2; i++) {
+                int segment_height = y2 - y1 + 1;
+                float a = (float) (i - y1) / height;
+                float b = (float) (i - y1) / segment_height;
+                int ax = x1 + (x3 - x1) * a;
+                int bx = x1 + (x2 - x1) * b;
+                if (ax > bx)
+                    std::swap(ax, bx);
+                //image.set(ax,i,red);
+                //image.set(bx,i,green);
+                for (int ii = ax; ii <= bx; ii++) {
+                    image.set(ii, i, color);
+                }
+            }
+            for (int j = y2; j <= y3; j++) {
+                int segment_height = y3 - y2 + 1;
+                float a = (float) (j - y1) / height;
+                float b = (float) (j - y2) / segment_height;
+                int ax = x1 + (x3 - x1) * a;
+                int bx = x2 + (x3 - x2) * b;
+                if (ax > bx)
+                    std::swap(ax, bx);
+                for (int jj = ax; jj <= bx; jj++) {
+                    image.set(jj, j, color);
+                }
+            }
+        }
+}
+
+void drawVertice(model m, TGAImage &image, TGAColor color){
+    std::cout << "nverts : " << m.nverts() << std::endl;
     for(int i = 1; i <= m.nverts(); i++){
         //std::cout << "ok"+i << std::endl;
         std::vector<float> vertice = m.vert(i);
         image.set(vertice[0]*(image.get_width()/2)+(image.get_width()/2), vertice[1]*(image.get_height()/2)+(image.get_height()/2), color);
     }
+}
+
+void drawTriangle(model m, TGAImage &image, TGAColor color){
     for(int j = 1; j <= m.nfaces(); j++){
+        //std::cout << j << "/" << m.nfaces() << std::endl;
         std::vector<int> face = m.face(j);
         int xA = m.vert(face[0])[0]*(image.get_width()/2)+(image.get_width()/2);
         int yA = m.vert(face[0])[1]*(image.get_height()/2)+(image.get_height()/2);
@@ -84,23 +134,17 @@ void draw(model m, TGAImage &image, TGAColor color){
         int yB = m.vert(face[1])[1]*(image.get_height()/2)+(image.get_height()/2);
         int xC = m.vert(face[2])[0]*(image.get_width()/2)+(image.get_width()/2);
         int yC = m.vert(face[2])[1]*(image.get_height()/2)+(image.get_height()/2);
-        line(xA,yA,xB,yB,image,color);
-        line(xB,yB,xC,yC,image,color);
-        line(xC,yC,xA,yA,image,color);
+        //line(xA,yA,xB,yB,image,color);
+        //line(xB,yB,xC,yC,image,color);
+        //line(xC,yC,xA,yA,image,color);
+        fillTriangle(xA,yA,xB,yB,xC,yC,image,TGAColor(rand()%255, rand()%255, rand()%255, 255));
+
     }
 }
 
-void fillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, TGAImage &image, TGAColor color){
-    std::vector<std::vector<int>> res1, res2, res3;
-    res1 = line(x1,y1,x2,y2, image, color);
-    res2 = line(x2,y2,x3,y3, image, color);
-    res3 = line(x3,y3,x1,y1, image, color);
-    for(int i = 0; i < res1[0].size(); i++)
-        line(x3,y3,res1[0][i],res1[1][i],image,color);
-    for(int i = 0; i < res2[0].size(); i++)
-        line(x1,y1,res2[0][i],res2[1][i],image,color);
-    for(int i = 0; i < res3[0].size(); i++)
-        line(x2,y2,res3[0][i],res3[1][i],image,color);
+void draw(model m, TGAImage &image, TGAColor color){
+    drawVertice(m,image,color);
+    drawTriangle(m,image,color);
 }
 
 int main(int argc, char** argv) {
@@ -110,10 +154,11 @@ int main(int argc, char** argv) {
         std::cout << tab[i] << std::endl;
     }
     */
+    int light[3] = {0,0,-1};
     model m("../african_head.obj");
-    TGAImage image(500,500, TGAImage::RGB);
-    draw(m,image, white);
-    //fillTriangle(10,10,400,100,200,400,image,red);
+    TGAImage image(1600,1600, TGAImage::RGB);
+    drawTriangle(m,image, white);
+    //fillTriangleTest(10,10,400,100,200,400,image,white);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("../output.tga");
     return 0;
